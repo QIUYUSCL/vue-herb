9cbe81049d75aa611c2c94e35e6e8df54e2bee28
sky <135691834+QIUYUSCL@users.noreply.github.com>
2025-07-03 16:56:17 +0800

    new

--- vue/src/utils/request.js
+++ vue/src/utils/request.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 创建 axios 实例
 const service = axios.create({
-    baseURL: 'http://localhost:8080', // 可根据实际后端接口地址配置，如 http://localhost:8080
+    baseURL: 'http://192.168.212.29:8080',
     timeout: 5000 // 请求超时时间
 });
 
